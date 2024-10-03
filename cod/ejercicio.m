ace1d2f3aa6e15c316845808bfb93b965cac24ea
bluke7ide <luisfernandoamey@gmail.com>
2024-10-03 16:17:27 -0600

    fix: 3i. Eran Lengendres

--- cod/ejercicio.m
+++ cod/ejercicio.m
@@ -3,33 +3,12 @@
 
 
 % ejercicio 3
-function [T, val, vec] = tortog(n)
+function [T, nod, w] = tortog(n)
     T = zeros(n+1);
-    % se sabe que b-a = 2 por lo que b0 = 0, entonces 
-    phis = cell(1, n);
-    phis{1} = @(x) 1;
-    phis{2} = @(x) x;
-    for i = 3:n+1
-        phis{i} = @(x) (phis{i-1}(x).*(2*i+1).*x-i.*phis{i-2}(x))./(i+1);
+    for k = 1:n
+        T(k,k+1) = sqrt(k/(k+1));
+        T(k+1,k) = T(k,k+1);
     end
-
-    norm = zeros(n+1, 1);
-    norm(1) = 1;
-    for i = 1:n
-        norm(i+1) = integral(@(x) phis{i+1}(x).^2, -1, 1);
-    end
-
-    T(1,1) = integral(@(x) x.*phis{1}(x).^2, -1, 1);
-
-    for i = 1:n
-        beta = (integral(@(x) x.*phis{i+1}(x).^2, -1, 1))/norm(i+1);
-        alpha = sqrt(norm(i+1)/norm(i));
-        T(i+1,i+1) = beta;
-        T(i,i+1) = alpha;
-        T(i+1,i) = T(i,i+1);
-    end
-
-    
     
     % w = zeros(n+1, 1);
     % for i = 1:n+1
@@ -37,7 +16,7 @@ function [T, val, vec] = tortog(n)
     % end
 
     
-    [vec, val] = eig(T);
+    [w, nod] = eig(T);
 end
 
-[T, val, vec] = tortog(2);
+[T, nod, w] = tortog(1);
